4e37191f835810f2b7b23d5bc7e3a65ca108b685
Nick Verhagen <nick_verhagen757@hotmail.com>
2018-11-09 13:26:20 +0100

    First interfaces

---
+++ Libary/Interfaces/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libary.Interfaces
+{
+    public interface IPerson
+    {
+        string Name { get; set; }
+        int age { get; set; }
+        string interested { get; set; }
+
+    }
+}
